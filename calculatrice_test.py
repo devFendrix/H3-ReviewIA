ff0ef9c13aac09c03b8b0d5a3b55745617ecdbb6
devFendrix <dev.fendrix@gmail.com>
2024-06-28 12:02:58 +0200

    add subtraction to project

--- calculatrice_test.py
+++ calculatrice_test.py
@@ -1,8 +1,14 @@
 import pytest
-from calculatrice import addition
+from calculatrice import addition, subtraction
 
 
 def test_addition():
     assert addition(2, 3) == 5
     assert addition(-1, 1) == 0
     assert addition(-1, -1) == -2
+
+
+def test_subtraction():
+    assert subtraction(5, 3) == 2
+    assert subtraction(3, 5) == -2
+    assert subtraction(-1, -1) == 0
